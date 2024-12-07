b13e5cf79f3af7722c85737f1b1066270b620df1
Andrew Larson <48035438+alarson892@users.noreply.github.com>
2024-12-07 15:19:00 -0600

    Init

---
+++ gamesite/src/main/java/com/gamesite/gamesite/GamesiteApplication.java
@@ -0,0 +1,13 @@
+package com.gamesite.gamesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesiteApplication.class, args);
+	}
+
+}
---
+++ gamesite/src/test/java/com/gamesite/gamesite/GamesiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesite.gamesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
